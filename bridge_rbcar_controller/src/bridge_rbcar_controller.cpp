14d1c2e83efe02b5f56bad35a028346848f690bc
racarla96 <racarla96@gmail.com>
2024-05-21 12:43:00 +0200

    Update code

--- bridge_rbcar_controller/src/bridge_rbcar_controller.cpp
+++ bridge_rbcar_controller/src/bridge_rbcar_controller.cpp
@@ -2,6 +2,8 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "ackermann_msgs/msg/ackermann_drive_stamped.hpp"
+#include "std_msgs/msg/float32.hpp"
+#include "sensor_msgs/msg/imu.hpp"
 using std::placeholders::_1;
 
 #include <cstdio>
@@ -9,18 +11,70 @@ using std::placeholders::_1;
 using json = nlohmann::json;
 
 #include <async-sockets/udpsocket.hpp>
+#include <async-sockets/udpserver.hpp>
 #include <iostream>
+#include <chrono>
+#include <mutex>
 
-class MinimalSubscriber : public rclcpp::Node
+class MinimalNode : public rclcpp::Node
 {
   private:
     UDPSocket<512> * udpSocket; // "true" to use Connection on UDP. Default is "false".
 
+    UDPServer<512> * udpServer_1;
+    UDPServer<512> * udpServer_2;
+
+    bool _ros_1_server_binded_1_ = true;
+    std::mutex mtx_1;
+    bool _ros_1_server_binded_2_ = true;
+    std::mutex mtx_2;
+
+    bool _ros_1_new_message_1_ = true;
+    json _pub_json_1_;
+    bool _ros_1_new_message_2_ = true;
+    json _pub_json_2_;
+
     std::string sub_topic;
     std::string ros_1_server_ip;
     uint16_t ros_1_server_port;
-    uint16_t ros_2_server_port;
+    std::string pub_topic_1;
+    uint16_t ros_2_server_port_1;
+    std::string pub_topic_2;
+    uint16_t ros_2_server_port_2;
     uint16_t Hz;
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr _pub_1_;
+    //rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _pub_2_;
+
+    void timer_callback()
+    {
+      if(_ros_1_new_message_1_){
+        auto message = std_msgs::msg::Float32();
+        mtx_1.lock();
+        message.data = _pub_json_1_["speed"];
+        _ros_1_new_message_1_ = false;
+        mtx_1.unlock();
+        _pub_1_->publish(message);
+      }
+      if(_ros_1_new_message_2_){
+        auto message = sensor_msgs::msg::Imu();
+        mtx_2.lock();
+        message.orientation.x = _pub_json_2_["orientation"]["x"];
+        message.orientation.y = _pub_json_2_["orientation"]["y"];
+        message.orientation.z = _pub_json_2_["orientation"]["z"];
+        message.orientation.w = _pub_json_2_["orientation"]["w"];
+        message.angular_velocity.x = _pub_json_2_["angular_velocity"]["x"];
+        message.angular_velocity.y = _pub_json_2_["angular_velocity"]["y"];
+        message.angular_velocity.z = _pub_json_2_["angular_velocity"]["z"];
+        message.linear_acceleration.x = _pub_json_2_["linear_acceleration"]["x"];
+        message.linear_acceleration.y = _pub_json_2_["linear_acceleration"]["y"];
+        message.linear_acceleration.z = _pub_json_2_["linear_acceleration"]["z"];
+        _ros_1_new_message_2_ = false;
+        mtx_2.unlock();
+        _pub_2_->publish(message);
+      }
+    }
 
     void _sub_callback_(const ackermann_msgs::msg::AckermannDriveStamped::SharedPtr _sub_msg_) const
     {
@@ -32,29 +86,76 @@ class MinimalSubscriber : public rclcpp::Node
     rclcpp::Subscription<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr _sub_;
 
   public:
-    MinimalSubscriber(const rclcpp::NodeOptions &options)
+    MinimalNode(const rclcpp::NodeOptions &options)
         : Node("bridge_rbcar_controller_node", options)
     {
         this->declare_parameter("sub_topic", "command");
         this->declare_parameter("ros_1_server_ip", "localhost");
         this->declare_parameter("ros_1_server_port", 8888);
-        this->declare_parameter("ros_2_server_port", 8889);
+        this->declare_parameter("pub_topic_1", "speed");
+        this->declare_parameter("ros_2_server_port_1", 8889);
+        this->declare_parameter("pub_topic_2", "speed");
+        this->declare_parameter("ros_2_server_port_2", 8889);
         this->declare_parameter("Hz", 25);
 
         this->get_parameter("sub_topic", sub_topic);
         this->get_parameter("ros_1_server_ip", ros_1_server_ip);
         this->get_parameter("ros_1_server_port", ros_1_server_port);
-        this->get_parameter("ros_2_server_port", ros_2_server_port);
+        this->declare_parameter("pub_topic_1", pub_topic_1);
+        this->declare_parameter("ros_2_server_port_1", ros_2_server_port_1);
+        this->declare_parameter("pub_topic_2", pub_topic_2);
+        this->declare_parameter("ros_2_server_port_2", ros_2_server_port_2);
         this->get_parameter("Hz", Hz);
 
-        _sub_ = this->create_subscription<ackermann_msgs::msg::AckermannDriveStamped>(
-        sub_topic, 1, std::bind(&MinimalSubscriber::_sub_callback_, this, _1));
+        _sub_ = this->create_subscription<ackermann_msgs::msg::AckermannDriveStamped>(sub_topic, 1, std::bind(&MinimalNode::_sub_callback_, this, _1));
 
         udpSocket = new UDPSocket<512>(true);
         udpSocket->Connect(ros_1_server_ip.c_str(), ros_1_server_port);
+
+        udpServer_1->Bind(_ros_1_server_port_1_, [](int errorCode, std::string errorMessage) {
+            _ros_1_server_binded_1_ = false; // Error binding socket
+        });
+        if (!_ros_1_server_binded_1_) 
+        { 
+          //RCLCPP_ERROR("No se ha podido iniciar correctamente el servidor UDP - ha fallado el BINDING - posiblemente puerto ocupado por otro proceso."); 
+          return -1;
+        }
+
+        udpServer_2->Bind(_ros_1_server_port_2_, [](int errorCode, std::string errorMessage) {
+            _ros_1_server_binded_2_ = false; // Error binding socket
+        });
+        if (!_ros_1_server_binded_2_) 
+        { 
+          //RCLCPP_ERROR("No se ha podido iniciar correctamente el servidor UDP - ha fallado el BINDING - posiblemente puerto ocupado por otro proceso."); 
+          return -1;
+        }
+
+        udpServer_1->onRawMessageReceived = [&](const char* message, int length, std::string ipv4, uint16_t port) {
+            mtx_1.lock();
+            _pub_json_1_ = json::parse(message);
+            _ros_1_new_message_1_ = true;
+            mtx_1.unlock();
+        };
+
+        udpServer_2->onRawMessageReceived = [&](const char* message, int length, std::string ipv4, uint16_t port) {
+            mtx_2.lock();
+            _pub_json_2_ = json::parse(message);
+            _ros_1_new_message_2_ = true;
+            mtx_2.unlock();
+        };
+
+        // Calcular el período en segundos como un double
+        double period_seconds = 1.0 / Hz;
+
+        // Convertir el período en microsegundos para crear una std::chrono::duration
+        auto period_duration = std::chrono::duration<double>(period_seconds);
+
+        _pub_1_ = this->create_publisher<std_msgs::msg::Float32>(pub_topic_1, 1);
+        _pub_2_ = this->create_publisher<sensor_msgs::msg::Imu>(pub_topic_2, 1);
+        timer_ = this->create_wall_timer(period_duration, std::bind(&MinimalPublisher::timer_callback, this));
     }
   
-    ~MinimalSubscriber()
+    ~MinimalNode()
     {
       udpSocket->Close();
     }
@@ -64,7 +165,7 @@ int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::NodeOptions options;
-  auto node = std::make_shared<MinimalSubscriber>(options);
+  auto node = std::make_shared<MinimalNode>(options);
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
